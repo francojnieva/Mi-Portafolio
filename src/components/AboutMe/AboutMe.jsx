38535cbf46632ebf898db1e1d200574662cdb4ef
francojnieva <franco97nieva@hotmail.com>
2024-10-02 20:15:09 -0300

    update about me section and footer

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,10 +1,10 @@
 
 const AboutMe = () => {
     return (
-        <section className='font-medium -mt-40 pb-3 text-lg space-y-6 text-[#808080] px-6 lg:pb-28 lg:w-[85%] lg:mx-auto lg:px-0 lg:-mt-32 lg:text-xl'>
-            <h1>Soy<span className=' text-[#00D8F0] font-bold'> Franco Javier Nieva </span>, de la provincia de Tucumán, Argentina. Apasionado por la tecnología, con un enfoque especial en el desarrollo web Frontend.</h1>
+        <section className='font-medium -mt-40 pb-3 text-lg space-y-6 text-[#808080] px-6 lg:pb-28 lg:w-[85%] lg:mx-auto lg:px-14 lg:-mt-32 lg:text-xl'>
+            <p>Soy<span className=' text-[#0080CA] font-bold'> Franco Javier Nieva </span> y me apasiona el mundo de la tecnología, especialmente el desarrollo web Front End.</p>
             <p>Responsable, organizado y empático, con un fuerte compromiso hacia el trabajo en equipo y una actitud proactiva hacia el aprendizaje continuo.</p>
-            <p>Me apasiona la creatividad y la resolución de problemas que implica el desarrollo Frontend. Estoy siempre dispuesto a enfrentar nuevos desafíos con entusiasmo y determinación.</p>
+            <p>Me apasiona la creatividad y la resolución de problemas que implica el desarrollo Front End. Estoy siempre dispuesto a enfrentar nuevos desafíos con entusiasmo y determinación.</p>
         </section>
     )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,23 +8,20 @@ const Footer = () => {
 		<footer className="footer px-6 py-10 font-medium bg-[#000] text-[#a0a0a0] flex flex-col lg:px-0 lg:w-[85%] mx-auto">
 			<section>
 				<h6 className="text-lg text-[#fff] mb-4">Página</h6>
-				<nav className=' flex flex-col space-y-6 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-8'>
-					<a href='#home' className="hover:text-[#fff] text-base transition-all">Inicio</a>
-					<a href='#aboutMe' className="hover:text-[#fff] text-base transition-all">Sobre mí</a>
-					<a href='#hability' className="hover:text-[#fff] text-base transition-all">Habilidades y Conocimientos</a>
-					<a href='#projects' className="hover:text-[#fff] text-base transition-all">Proyectos</a>
-				</nav>
-			</section>
-			<section className=" flex flex-col-reverse w-full lg:flex-row justify-between lg:items-center">
-				<aside className="text-center pt-8 lg:pt-0">
-					<p>Copyright © 2024 - All right reserved</p>
-				</aside>
-				<nav className=" flex items-baseline space-x-4 text-xl lg:text-2xl">
-					<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://www.linkedin.com/in/francojnieva/' target='_blanck'><FaLinkedin /></a>
-					<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://github.com/francojnieva' target='_blanck'><FaGithub /></a>
-					<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://instagram.com/franconieva.97' target='_blanck'><RiInstagramFill /></a>
-					<a className='hover:text-[#fff] transition duration-500 ease-out' href='mailto:franco97nieva@gmail.com' target='_blanck'><MdEmail /></a>
-				</nav>
+				<section className=" space-y-12">
+					<nav className=' flex flex-col space-y-6 lg:flex-row lg:items-center lg:space-y-0 lg:space-x-8'>
+						<a href='#home' className="hover:text-[#fff] text-base transition-all">Inicio</a>
+						<a href='#aboutMe' className="hover:text-[#fff] text-base transition-all">Sobre mí</a>
+						<a href='#hability' className="hover:text-[#fff] text-base transition-all">Habilidades y Conocimientos</a>
+						<a href='#projects' className="hover:text-[#fff] text-base transition-all">Proyectos</a>
+					</nav>
+					<nav className=" flex items-baseline space-x-4 text-xl lg:text-2xl">
+						<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://www.linkedin.com/in/francojnieva/' target='_blanck'><FaLinkedin /></a>
+						<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://github.com/francojnieva' target='_blanck'><FaGithub /></a>
+						<a className='hover:text-[#fff] transition duration-500 ease-out' href='https://instagram.com/franconieva.97' target='_blanck'><RiInstagramFill /></a>
+						<a className='hover:text-[#fff] transition duration-500 ease-out' href='mailto:franco97nieva@gmail.com' target='_blanck'><MdEmail /></a>
+					</nav>
+				</section>
 			</section>
 		</footer>
 	)
